81e801712b0c9a8260449204c6dae17b7bb19277
Fatih AKYAR <akyar.fatih@yahoo.com>
2023-04-09 07:52:58 +0100

    L6NumberOfDiscIntersections. solved via 100
    In this example I learned an example of integer overflow

--- codilityL6NumberOfDiscIntersections.cpp
+++ codilityL6NumberOfDiscIntersections.cpp
@@ -7,21 +7,72 @@
 using namespace std;
 //Print function
 void print_vector(vector<int> &vect)
-   {   cout<<"\nPrinted vector is: \n";
+   {   //cout<<"\nPrinted vector is: \n";
        for (auto i = vect.begin(); i != vect.end(); ++i)
         {cout<< *i << " ";}
+        cout<<endl;
    }
 int solution(vector<int> A)
 {
-    int last=A.size();
-    sort(A.begin(),A.end());
-    int result=max(A[0]*A[1]*A[last-1],A[last-3]*A[last-2]*A[last-1]);
+    long last=A.size();
+    long result=0,active=0;
+    vector<long> open;
+    vector<long> close;
+    //disc open
+    for(long i=0;i<last;i++)
+    {
+//        cout<<"A[i] is "<<A[i]<<"  i is"<<i<<endl;
+        close.push_back(i-A[i]);
+        open.push_back(A[i]+i);
+    }
+//    cout<<"open is ";
+//    print_vector(open);
+//    cout<<"close is ";
+//    print_vector(close);
+//    cout<<endl<<endl;
+
+    sort(open.begin(),open.end());
+    sort(close.begin(),close.end());
+//    cout<<"Sorted open is ";
+//    print_vector(open);
+//    cout<<"Sorted close is ";
+//    print_vector(close);
+//    cout<<endl<<endl;
+
+    while(!open.empty())
+    {
+        if(open.back()>=close.back())
+        {
+//            cout<<"Open before operation"<<endl;
+//            print_vector(open);
+            result=result+active;
+            active++;
+            open.pop_back();
+//            cout<<"Open after operation"<<endl;
+//            print_vector(open);
+
+        }else
+        {
+//            cout<<"Close before operation"<<endl;
+//            print_vector(close);
+            active--;
+            close.pop_back();
+//            cout<<"Close after operation"<<endl;
+//            print_vector(close);
+        }
+//        cout<<"Result is now "<<result<<endl;
+//        cout<<"Active is now "<<active<<endl<<endl;
+    if(result>10000000){return -1;}
+    }
+    //disc close
+
     return result;
 }
 
 int main ()
     {
-      int myints[] = {3,4,4,-6,1,-4,4};
+      //int myints[] = {1,5,2,1,4,0};
+      int myints[] = {};
       vector<int> myvector (myints, myints+sizeof(myints)/sizeof(myints[0]) );
       string S{"CA"};
       //print_vector(result);
